3300aab1303c84a710fbe1d436845f1571584603
Fernando Cortes <fernando.minero@gmail.com>
2022-06-18 14:42:11 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import { Menu, Container } from '@mui/material';
+
+
+function App() {
+    return (
+        <Container>
+            <Menu></Menu>
+            <Principal>
+                <Palavra id='1'>
+                    <Letra></Letra>
+                    <Letra></Letra>
+                    <Letra></Letra>
+                    <Letra></Letra>
+                    <Letra></Letra>
+                </Palavra>
+                <Palavra id='2'>
+                    <Letra></Letra>
+                    <Letra></Letra>
+                    <Letra></Letra>
+                    <Letra></Letra>
+                    <Letra></Letra>
+                </Palavra>
+                <Palavra id='3'>
+                    <Letra></Letra>
+                    <Letra></Letra>
+                    <Letra></Letra>
+                    <Letra></Letra>
+                    <Letra></Letra>
+                </Palavra>
+                <Palavra id='4'>
+                    <Letra></Letra>
+                    <Letra></Letra>
+                    <Letra></Letra>
+                    <Letra></Letra>
+                    <Letra></Letra>
+                </Palavra>
+                <Palavra id='5'>
+                    <Letra></Letra>
+                    <Letra></Letra>
+                    <Letra></Letra>
+                    <Letra></Letra>
+                    <Letra></Letra>
+                </Palavra>
+                <Palavra id='6'>
+                    <Letra></Letra>
+                    <Letra></Letra>
+                    <Letra></Letra>
+                    <Letra></Letra>
+                    <Letra></Letra>
+                </Palavra>
+            </Principal>
+            <Teclado></Teclado>
+        </Container>
+    );
+}
+
+export default App;
